af057fc0d53cd425bfaa912dc15ec7cd5fbc9599
Jono Yang <jyang@nexb.com>
2023-10-18 18:39:06 -0700

    Fix variables #943
    Signed-off-by: Jono Yang <jyang@nexb.com>

--- scanpipe/pipes/d2d.py
+++ scanpipe/pipes/d2d.py
@@ -585,7 +585,7 @@ def most_similar_match(directory_matches):
     highest_similarity_score = 0
     most_similar_match = None
     for match in directory_matches:
-        similarity_score = match.similarity_score
+        similarity_score = match.get('similarity_score')
         if similarity_score > highest_similarity_score:
             most_similar_match = match
             highest_similarity_score = similarity_score
@@ -803,8 +803,8 @@ def match_purldb_directories_post_process(project, logger=None):
 
         best_matched_package = None
         best_matched_package_percentage = 0.0
-        for package in directory.discovered_packages.all():
-            directories = resources_by_filenames_by_purls[package.purl][directory.name]
+        for package in matched_directory.discovered_packages.all():
+            directories = resources_by_filenames_by_purls[package.purl][matched_directory.name]
 
             for directory in directories:
                 directory_path = directory.get('path')
